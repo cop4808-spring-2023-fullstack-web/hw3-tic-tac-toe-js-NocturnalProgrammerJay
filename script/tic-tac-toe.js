b51a1fd7403d956f8525bc880b1424601f4eca1f
Jamar Andrade <jandrade2018@fau.edu>
2023-02-11 18:30:48 -0500

    Implemented tile color change depending on the winner.

--- script/tic-tac-toe.js
+++ script/tic-tac-toe.js
@@ -1,51 +1,54 @@
-const statusDisplay = document.querySelector(".status");
+/* prettier-ignore */
 
-const randomPlayerChoice = () => Math.floor(Math.random() * 10 + 1);
+const statusDisplay = document.querySelector(".status")
 
-let gameActive = true;
-let currentPlayer = randomPlayerChoice() % 2 == 0 ? "X" : "O";
-let gameState = ["", "", "", "", "", "", "", "", ""];
+const randomPlayerChoice = () => Math.floor(Math.random() * 10 + 1)
 
-let player1 = currentPlayer;
-let player2 = currentPlayer === "X" ? "O" : "X";
+let gameActive = true
+let currentPlayer = randomPlayerChoice() % 2 == 0 ? "X" : "O"
+let gameState = ["", "", "", "", "", "", "", "", ""]
 
-console.log(player1, player2);
+let player1 = currentPlayer
+let player2 = currentPlayer === "X" ? "O" : "X"
 
-const scoreBoard = { player1: 0, player2: 0 };
+console.log(player1, player2)
+
+const scoreBoard = { player1: 0, player2: 0 }
 
 // Build status bar
 // const board = document.createElement("div").appendChild(statusBar);
-const statusBar = document.querySelector(".players");
+const statusBar = document.querySelector(".players")
+
+let player1Color
+let player2Color
 
 const buildScoreBoard = () => {
-  let player1Color;
-  let player2Color;
 
   if (currentPlayer === player1) {
-    player1Color = "blue";
-    player2Color = "";
+    player1Color = "blue"
+    player2Color = ""
   }
 
   if (currentPlayer === player2) {
-    player1Color = "";
-    player2Color = "red";
+    player1Color = ""
+    player2Color = "red"
   }
 
-  statusBar.style.height = "50px";
-  statusBar.style.padding = "5px";
-  statusBar.style.border = "solid #414141";
-  statusBar.style.width = statusBar.previousSibling.width;
-  statusBar.style.borderRadius = "5%";
-  statusBar.innerHTML = `<p> <b style="color:${player1Color}">Player 1:</b> ${scoreBoard.player1} <b>|</b> <b style="color:${player2Color}">Player 2:</b> ${scoreBoard.player2} </p>`;
-};
+  statusBar.style.height = "50px"
+  statusBar.style.padding = "5px"
+  statusBar.style.border = "solid #414141"
+  statusBar.style.width = statusBar.previousSibling.width
+  statusBar.style.borderRadius = "5%"
+  statusBar.innerHTML = `<p> <b style="color:${player1Color}">Player 1:</b> ${scoreBoard.player1} <b>|</b> <b style="color:${player2Color}">Player 2:</b> ${scoreBoard.player2} </p>`
+}
 
-buildScoreBoard();
+buildScoreBoard()
 
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const winningMessage = () => `Player ${currentPlayer} has won!`
+const drawMessage = () => `Game ended in a draw!`
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`
 
-statusDisplay.innerHTML = currentPlayerTurn();
+statusDisplay.innerHTML = currentPlayerTurn()
 
 const winningConditions = [
   [0, 1, 2],
@@ -56,92 +59,109 @@ const winningConditions = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-];
+]
 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
-  gameState[clickedCellIndex] = currentPlayer;
-  clickedCell.innerHTML = currentPlayer;
+  gameState[clickedCellIndex] = currentPlayer
+  clickedCell.innerHTML = currentPlayer
 }
 
 function handlePlayerChange() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  statusDisplay.innerHTML = currentPlayerTurn();
-  buildScoreBoard();
+  currentPlayer = currentPlayer === "X" ? "O" : "X"
+  statusDisplay.innerHTML = currentPlayerTurn()
+  buildScoreBoard()
 }
 
 function handleResultValidation() {
-  let roundWon = false;
+  let roundWon = false
   for (let i = 0; i <= 7; i++) {
-    const winCondition = winningConditions[i];
-    let a = gameState[winCondition[0]];
-    let b = gameState[winCondition[1]];
-    let c = gameState[winCondition[2]];
+    const winCondition = winningConditions[i]
+    let a = gameState[winCondition[0]]
+    let b = gameState[winCondition[1]]
+    let c = gameState[winCondition[2]]
     if (a === "" || b === "" || c === "") {
-      continue;
+      continue
     }
     if (a === b && b === c) {
-      roundWon = true;
-      break;
+      roundWon = true
+
+      // highlight tiles
+      document.querySelectorAll(".cell").forEach((cell, idx) => {
+        if (
+          cell.getAttribute("data-cell-index") === winCondition[0].toString() ||
+          cell.getAttribute("data-cell-index") === winCondition[1].toString() ||
+          cell.getAttribute("data-cell-index") === winCondition[2].toString()
+        ) {
+          console.log(document.querySelector(`.cell${idx.toString()}`))
+          document.querySelector(`.cell${idx.toString()}`).style.zIndex = 1
+          // document.querySelector(`.cell${idx.toString()}`).style.border = "6px solid rgb(251,100,204)"
+          document.querySelector(`.cell${idx.toString()}`).style.border = currentPlayer === player1 ? `6px solid ${player1Color}` : `6px solid ${player2Color}`
+        }
+      })
+      break
     }
   }
 
   if (roundWon) {
     if (currentPlayer === player1) {
-      scoreBoard.player1 += 1;
+      scoreBoard.player1 += 1
     }
 
     if (currentPlayer === player2) {
-      scoreBoard.player2 += 1;
+      scoreBoard.player2 += 1
     }
 
-    statusDisplay.innerHTML = winningMessage();
-    gameActive = false;
-    statusDisplay.style.color = "rgb(251,100,204)";
-    buildScoreBoard();
-    return;
+    statusDisplay.innerHTML = winningMessage()
+    gameActive = false
+    statusDisplay.style.color = "rgb(251,100,204)"
+    buildScoreBoard()
+    return
   }
 
-  let roundDraw = !gameState.includes("");
+  let roundDraw = !gameState.includes("")
 
   if (roundDraw) {
-    statusDisplay.innerHTML = drawMessage();
-    gameActive = false;
-    statusDisplay.style.color = "rgb(251,100,204)";
-    buildScoreBoard();
-    return;
+    statusDisplay.innerHTML = drawMessage()
+    gameActive = false
+    statusDisplay.style.color = "rgb(251,100,204)"
+    buildScoreBoard()
+    return
   }
 
-  handlePlayerChange();
+  handlePlayerChange()
 }
 
 function handleCellClick(clickedCellEvent) {
-  const clickedCell = clickedCellEvent.target;
+  const clickedCell = clickedCellEvent.target
   const clickedCellIndex = parseInt(
     clickedCell.getAttribute("data-cell-index")
-  );
+  )
 
   if (gameState[clickedCellIndex] !== "" || !gameActive) {
-    return;
+    return
   }
 
-  handleCellPlayed(clickedCell, clickedCellIndex);
-  handleResultValidation();
+  handleCellPlayed(clickedCell, clickedCellIndex)
+  handleResultValidation()
 }
 
 function handleRestartGame() {
-  gameActive = true;
-  currentPlayer = "X";
-  gameState = ["", "", "", "", "", "", "", "", ""];
+  gameActive = true
+  currentPlayer = "X"
+  gameState = ["", "", "", "", "", "", "", "", ""]
 
-  statusDisplay.style.color = "rgb(65, 65, 65)";
+  statusDisplay.style.color = "rgb(65, 65, 65)"
 
-  // currentPlayer = randomPlayerChoice() % 2 == 0 ? "X" : "O";
-
-  statusDisplay.innerHTML = currentPlayerTurn();
-  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+  statusDisplay.innerHTML = currentPlayerTurn()
+  document.querySelectorAll(".cell").forEach((cell) => {
+    cell.innerHTML = ""
+    cell.style.border = "6px solid rgb(65, 65, 65)"
+  })
 }
+
+console.log(document.querySelectorAll(".cell"))
 
 document
   .querySelectorAll(".cell")
-  .forEach((cell) => cell.addEventListener("click", handleCellClick));
-document.querySelector(".restart").addEventListener("click", handleRestartGame);
+  .forEach((cell) => cell.addEventListener("click", handleCellClick))
+document.querySelector(".restart").addEventListener("click", handleRestartGame)
